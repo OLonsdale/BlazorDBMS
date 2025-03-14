2a5b9f413e50e0be533a000b35fda16d46514ea2
Oliver Lonsdale <oliver.lonsdale@fourquant.com>
2025-03-14 21:49:23 +0000

    Working basic implementation

--- BlazorDBMS/Program.cs
+++ BlazorDBMS/Program.cs
@@ -1,4 +1,5 @@
 using BlazorDBMS.Components;
+using MudBlazor.Services;
 
 namespace BlazorDBMS;
 
@@ -11,6 +12,8 @@ public class Program
         // Add services to the container.
         builder.Services.AddRazorComponents()
             .AddInteractiveServerComponents();
+        
+        builder.Services.AddMudServices();
 
         var app = builder.Build();
 
